71be2dd29366a1ef19b45d21a8a3bc46b1bb0d40
dima.alan.te_edu.zst <dima.alan.te_edu.zst@30NKomp15>
2025-09-08 12:20:02 +0200

    Dodaj pliki projektów.

---
+++ MVVMPokeDexPal/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MVVMPokeDexPal
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MVVMPokeDexPal/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Foundation;
+using MVVMPokeDexPal.ViewModel;
+
+namespace MVVMPokeDexPal
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
